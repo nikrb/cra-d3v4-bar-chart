e895ee2d4d1bfa3ef107f745e2c34a8dab1f5932
nikrb <nik_rb@yahoo.com>
2017-04-22 00:26:59 +0100

    create the bars

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as d3 from 'd3';
+import {all, filtered} from './data';
 
 class App extends Component {
+  state = {
+    data: filtered
+  };
   render() {
+    const width = 400;
+    const barHeight = 20;
+
+    const x = d3.scaleLinear()
+      .domain( [0, d3.max( this.state.data, d => d.total)])
+      .range( [0, width]);
+
+    const bars = this.state.data.map( ( d, ndx) => {
+      return (
+        <rect key={ndx} x={0} y={ndx*barHeight} width={x(d.total)} height={barHeight-1}/>
+      );
+    });
     return (
       <div className="App">
         <div className="App-header">
@@ -13,6 +30,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <svg className="chart" width={width} height={barHeight * this.state.data.length} >
+          {bars}
+        </svg>
       </div>
     );
   }
