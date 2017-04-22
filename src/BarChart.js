5608a487fc4ae33e203d138808da5a8200bd7b09
nikrb <nik_rb@yahoo.com>
2017-04-22 20:40:05 +0100

    add tooltip styles

--- src/BarChart.js
+++ src/BarChart.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
-export default class HorizBarChart extends React.Component {
+export default class BarChart extends React.Component {
+  handleMouseOver = (e) => {
+
+  };
+  handleMouseLeave = (e) => {
+
+  };
   render = () => {
     const {data, xScale, yScale, height} = this.props;
     const bars = data.map( ( d, ndx) => {
       return (
         <rect key={ndx} x={xScale( d.name)} y={yScale( d.total)}
           width={xScale.bandwidth()-1}
-          height={height - yScale(d.total)}/>
+          height={height - yScale(d.total)}
+          onMouseOver={this.handleMouseOver}
+          onMouseLeave={this.handleMouseLeave}/>
       );
     });
     // const text = data.map( (d, ndx) => {
