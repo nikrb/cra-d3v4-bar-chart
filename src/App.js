986ed453263d5072b82f2df6e5d72c1884d527ee
nikrb <nik_rb@yahoo.com>
2017-04-22 10:35:32 +0100

    scale x ordinal

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import * as d3 from 'd3';
 import {all, filtered} from './data';
@@ -12,16 +12,21 @@ class App extends Component {
   render = () => {
     const width = 600;
     const height = 400;
-    const barWidth = width/this.state.data.length;
+    // const barWidth = width/this.state.data.length;
 
     const yScale = d3.scaleLinear()
       .domain( [0, d3.max( this.state.data, d => d.total)])
       .range( [height, 0]);
 
+    const xScale = d3.scaleBand()
+      .domain( this.state.data.map( (d) => d.name))
+      .range( [0, width]);
+
     return (
       <div className="App">
         <svg className="chart" width={width} height={height} >
-          <HorizBarChart height={height} barWidth={barWidth} data={this.state.data} yScale={yScale} />
+          <HorizBarChart height={height}
+            data={this.state.data} xScale={xScale} yScale={yScale} />
         </svg>
       </div>
     );
--- src/HorizBarChart.js
+++ src/HorizBarChart.js
@@ -2,17 +2,17 @@ import React from 'react';
 
 export default class HorizBarChart extends React.Component {
   render = () => {
-    const {data, barWidth, yScale, height} = this.props;
+    const {data, xScale, yScale, height} = this.props;
     const bars = data.map( ( d, ndx) => {
       return (
-        <rect key={ndx} x={ndx*barWidth} y={yScale( d.total)}
-          width={barWidth-1}
+        <rect key={ndx} x={xScale( d.name)} y={yScale( d.total)}
+          width={xScale.bandwidth()}
           height={height - yScale(d.total)}/>
       );
     });
     const text = data.map( (d, ndx) => {
       return (
-        <text key={ndx} x={barWidth*(ndx+1)-5} y={height-(height-yScale( d.total))} dy={"1em"} >
+        <text key={ndx} x={xScale( d.name)+xScale.bandwidth()} y={yScale( d.total)+3} dy={"1em"} >
           {d.name}
         </text>
       );
